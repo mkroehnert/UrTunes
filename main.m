317c7e180444df503e01e03cdb82568682085e65
mkroehnert <mkroehnert@users.sourceforge.net>
2010-09-01 10:33:30 +0200

    initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  UrTunes
+//
+//  Created by MK on 01.09.10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
