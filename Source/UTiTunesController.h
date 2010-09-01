85e147522258d9dca461bee3fd2f9c567bcc56e0
mkroehnert <mkroehnert@users.sourceforge.net>
2010-09-02 01:39:45 +0200

    UTiTunesController: new class which is responsible for handling events from the panel

---
+++ Source/UTiTunesController.h
@@ -0,0 +1,19 @@
+//
+//  UTiTunesController.h
+//  UrTunes
+//
+//  Created by MK on 01.09.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "iTunes.h"
+#import <Cocoa/Cocoa.h>
+
+
+@interface UTiTunesController : NSObject {
+    iTunesApplication* iTunes;
+}
+
+- (IBAction) log:(id)sender;
+
+@end
---
+++ Source/UTiTunesController.m
@@ -0,0 +1,35 @@
+//
+//  UTiTunesController.m
+//  UrTunes
+//
+//  Created by MK on 01.09.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "UTiTunesController.h"
+
+
+@implementation UTiTunesController
+
+- (id) init
+{
+    self = [super init];
+    if (self)
+        iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+
+    return self;
+}
+
+- (IBAction) log:(id)sender
+{    
+    if ( [iTunes isRunning] ) {
+        if (iTunesEPlSPlaying == [iTunes playerState])
+            NSLog(@"Current song is %@", [[iTunes currentTrack] name]);
+        else
+            NSLog(@"iTunes is currently not playing");
+    }
+    else
+        NSLog(@"No iTunes Running");
+}
+
+@end
