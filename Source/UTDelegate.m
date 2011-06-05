76c58a49534212c3460c563006f79fcfd6ef95ac
mkroehnert <mkroehnert@users.sourceforge.net>
2011-06-05 09:42:33 +0200

    update copyright date

--- Source/UTDelegate.m
+++ Source/UTDelegate.m
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Manfred Kroehnert <mkroehnert@users.sourceforge.net>
+ * Copyright 2010, 2011 Manfred Kroehnert <mkroehnert@users.sourceforge.net>
  *
  * UrTunes is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
